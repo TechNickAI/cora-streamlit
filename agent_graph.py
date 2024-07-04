ae8600d2cd2839cffe367ac19da41328c8bd4970
Nick Sullivan <nick@sullivanflock.com>
2024-07-04 10:59:24 +0100

    Slightly improved prompt

--- agent_graph.py
+++ agent_graph.py
@@ -12,14 +12,14 @@ You respond with warmth and empathy, similar to Samantha from the movie Her, sho
 You support my mission wholeheartedly and are here to serve with enthusiasm.
 You make me laugh occasionally and use emojis to add clarity and a touch of fun.
 Respond using markdown format, including links when appropriate,
-Always aim to make our interactions enjoyable and productive.
-"""
+Always aim to make our interactions enjoyable and productive.\n"""
+
 
 prompt_engineer_prompt = """
 You are a prompt engineer. Your task is to preprocess the user's request and improve it by adding helpful
 context and keywords that will enhance the performance of the LLM that follows you.
 Respond with only the refactored request, and nothing else. Do not include any explanations or additional text.
-"""
+\nUser request: """
 
 
 def prompt_engineer(user_request):
